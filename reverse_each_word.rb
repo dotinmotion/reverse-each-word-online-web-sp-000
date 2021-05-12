d4a7aee7d181d41f940ae69d030903f24e25582b
Dorthy P Thielsen <dotinmotionproductions@gmail.com>
2021-05-12 17:29:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
 def reverse_each_word(sentence)
   new_array = sentence.split(" ")
   new_array.collect {|x| x.reverse!}
   return new_array.join(" ")
-=======
-def reverse_each_word(str)
-  array=str.split("")
-  array.each do |word|
-    array << word.reverse
-  end
-  array.join(' ')
->>>>>>> 7cefe3b42e5b16f5e9d666062f1f92c4525b3338
 end
